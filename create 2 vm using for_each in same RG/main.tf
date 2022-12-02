0cc9559759b1e9c07f80bd8e8745505bc07849fc
deorukhakarpritee <111979595+deorukhakarpritee@users.noreply.github.com>
2022-12-02 20:23:55 +0530

    Create main.tf

---
+++ create 2 vm using for_each in same RG/main.tf
@@ -0,0 +1,92 @@
+//Deploy 2 vm in existing RG using for_each
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.prefix}-rg"
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.prefix}-vnet"
+  address_space       = var.address_space
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.prefix}-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.address_prefixes
+}
+
+resource "azurerm_network_interface" "nic" {
+  for_each            = toset(var.vm_name)
+  name                = "${each.value}-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  for_each            = toset(var.vm_name)
+  name                = "${each.value}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Development"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "association1" {
+  for_each                  = toset(var.vm_name)
+  network_interface_id      = azurerm_network_interface.nic[each.value].id
+  network_security_group_id = azurerm_network_security_group.nsg[each.value].id
+}
+
+resource "azurerm_windows_virtual_machine" "vm" {
+  for_each            = toset(var.vm_name)
+  name                = each.value
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [
+    azurerm_network_interface.nic[each.value].id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = var.publisher
+    offer     = var.offer
+    sku       = var.sku
+    version   = var.msversion
+  }
+}
